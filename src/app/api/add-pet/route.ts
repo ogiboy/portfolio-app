dfd45a79036b12a89d18ecd6fbc2eb268e6306ab
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-03-19 02:51:15 +0300

    add-pet now works

--- src/app/api/add-pet/route.ts
+++ src/app/api/add-pet/route.ts
@@ -5,7 +5,9 @@ export async function POST(request: Request) {
   try {
     const { id, petName, ownerName } = await request.json()
 
-    await sql`INSERT INTO pets (id, name, owner) VALUES ('${id}', '${petName}', '${ownerName}')`
+    console.log('id: ' + id, 'petName: ' + petName, 'owner: ' + ownerName)
+
+    await sql`INSERT INTO pets (id, name, owner) VALUES (${id}, ${petName}, ${ownerName})`
 
     return new NextResponse('Pet added successfully', { status: 200 })
   } catch (error) {
--- src/app/api/see-pets/route.ts
+++ src/app/api/see-pets/route.ts
@@ -3,13 +3,15 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const result = await sql`SELECT * FROM pets`
+    const result = await sql`SELECT * FROM pets LIMIT 10`
 
     const pets = result.rows.map((row: any) => ({
-      id: crypto.randomUUID(),
+      id: row.id,
       name: row.name,
       owner: row.owner,
     }))
+
+    console.log(JSON.stringify(pets))
 
     return new NextResponse(JSON.stringify(pets), { status: 200 })
   } catch (error) {
